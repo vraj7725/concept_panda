347e5cca4187c583669070a10e5a92e823aec4b1
Vaibhav Raj <vraj7725@gmail.com>
2017-08-30 23:21:58 +0530

    solution 2

--- problemStatement.cpp
+++ problemStatement.cpp
@@ -1,32 +1,110 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+int countt;
+// this is stack for storing data pairwise 
+stack<pair<int, int>> s;
+//c1 - 5
+//c2 - 3
+
+int arr[6][4];
+
+//bool function return always 1 ya 0 
+bool getSol(int c1, int c2) {
+	if(c1 + c2 == 4)
+		return true;
+	if(arr[c1][c2])
+		return false;
+	arr[c1][c2] = 1;
+	// cout<<c1<<" "<<c2<<endl;
+	if(c1 && !c2) {
+		int t1 = 0, t2 = 0;
+		t1 = c1;
+		t2 = min(3,t1);
+		t1 -= t2;
+		if (getSol(t1,t2)) {
+		 	s.push(make_pair(1,2));
+			return true;
+		}
+	}
+	if(c1 && !c2) {
+		int t1 = 0, t2 = 0;
+		t2 = c2;
+		t1 = min(5,t2);
+		t2 -= t1;
+		if (getSol(t1,t2)) {
+			s.push(make_pair(2,1));
+			return true;
+		}
+	}
+	if (c1 && c2) {
+		int t1 = 0, t2 = 0;
+		t1 = c1;
+		t2 = c2;
+		// cout<<"Here\n";
+		int temp = min(t1, 3-t2);
+		t2 += temp;
+		t1 -= temp;
+		if (getSol(t1,t2)) {
+			s.push(make_pair(2,1));
+			return true;
+		}
+		temp = min(t2,5-t1);
+		t1 += temp;
+		t2 -= temp;
+		if (getSol(t1,t2)) {
+			s.push(make_pair(1,2));
+			return true;
+		}	
+	}
+	if (c1!=5) {
+		if (getSol(5,c2)) {
+			s.push(make_pair(3,1));
+			return true;
+		}
+	}
+	if (c2!=3) {
+		if (getSol(c1,3)) {
+			s.push(make_pair(3,2));
+			return true;
+		}
+	}
+	
+	if (c1) {   
+		if (getSol(0,c2)) {
+			s.push(make_pair(1,3));
+			return true;
+		}
+	}
+	if (c2) {   
+		if (getSol(c1,0)) {
+			s.push(make_pair(1,3));
+			return true;
+		}
+	}
+	return false;
+}
 
 int main() {
-  int volumeOfA = 5, volumeOfB = 3, volumeToBeCalculated = 4, count = 0, tempVolume = volumeOfB, volumeLeftInA = 0, volumeLeftInB = 0;
+	/* this is basically 2d matrix for retrieving the values */
+	int cup1 = 0, cup2 = 0;
+	for(int i = 0; i < 6; i ++) { 
+		for(int j = 0; j < 4; j ++)
+			arr[i][j] = 0;
+	}
+	// string for printing 
+	string sa[3];
+	sa[0] = "Cup1";
+	sa[1] = "Cup2";
+	sa[2] = "Sink";
 
-  do {
-    count ++;
-    if (count == 1) {
-     cout << "pouring water to cup a and transferring to cup b \n";
-    } else {
-     cout << "pouring water to cup a \n";
-    }
-    
-    if ( volumeOfA == volumeToBeCalculated || volumeOfB == volumeToBeCalculated) {
-      break;
-    }
-
-    volumeLeftInA = volumeOfA  - tempVolume;
-    count ++;
-    if ( volumeLeftInA <= volumeOfA - volumeOfB) {
-        cout <<"emptying cup b \n";
-    }
-    volumeLeftInB = volumeLeftInA;
-    cout<<"Transferring water left in A to B \n";
-    tempVolume = volumeOfB - volumeLeftInB;
-    count ++;
-  }while (volumeLeftInA != volumeToBeCalculated || volumeLeftInB != volumeToBeCalculated);
-
-  cout<<"Volume of container is " << volumeToBeCalculated << "ml";
+	bool f = getSol(0,0);
+	// this is for result 
+	while(!s.empty()) {
+		cout<<sa[s.top().first-1]<<" "<<sa[s.top().second-1]<<endl;
+		countt++;
+		//pop for deleting  top most element from stack
+		s.pop();
+	}
+	cout<<"finally water level is "<<countt-3<<endl;
+	return 0;
 }
